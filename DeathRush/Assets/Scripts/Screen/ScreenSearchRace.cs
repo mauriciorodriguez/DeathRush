3904cdcfea451d815e0066409c99c9150ec15012
Martín Letizia <martin.letiziaa@gmail.com>
2017-11-18 15:07:51 -0300

    Animación de puerta abriéndose (Bunker)

--- DeathRush/Assets/Scripts/Screen/ScreenSearchRace.cs
+++ DeathRush/Assets/Scripts/Screen/ScreenSearchRace.cs
@@ -16,6 +16,13 @@ public class ScreenSearchRace : ScreenView
     private int _totalPilots;
     private List<GameObject> _vehiclesList = new List<GameObject>();
     private PlayerData _playerData;
+    public Animation[] lockers;
+    public Animation[] doorBunker;
+    public Animation lockersRotation;
+    private float _timer = 10;
+    public Material lockerColor;
+
+    private int _track;
 
     private void OnEnable()
     {
@@ -23,6 +30,7 @@ public class ScreenSearchRace : ScreenView
         _playerData = PlayerData.instance;
         loading.SetActive(false);
         EnableTracks();
+        lockerColor.SetColor("_Color", Color.red);
     }
 
     private void EnableTracks()
@@ -39,33 +47,11 @@ public class ScreenSearchRace : ScreenView
         _totalPilots = _playerData.racerList.Count;
         if (_totalPilots > 0 && _playerData.selectedRacer != -1)
         {
+            _track = track;
             PersistendData.instance.ReleaseAllInfo();
-            if (track == 0)
-            {
-                loading.SetActive(true);
-                SceneManager.LoadScene((int)SCENES_NUMBER.DesertTrack);
-            }
-            else if (track == 1)
-            {
-                loading.SetActive(true);
-                SceneManager.LoadScene((int)SCENES_NUMBER.WaterTomb);
-            }
-            else if (track == 2)
-            {
-                loading.SetActive(true);
-                SceneManager.LoadScene((int)SCENES_NUMBER.RushHour);
-            }
-            else if (track == 3)
-            {
-                loading.SetActive(true);
-                SceneManager.LoadScene((int)SCENES_NUMBER.Satellitrack);
-            }
+            cameraMenu.setMount(cameraMenu.doorMount);
+            OpenDoor();
 
-            else if (track == 4)
-            {
-                loading.SetActive(true);
-                SceneManager.LoadScene((int)SCENES_NUMBER.InsideTheCore);
-            }
         }
         else if (_totalPilots == 0)
         {
@@ -76,4 +62,70 @@ public class ScreenSearchRace : ScreenView
             OnNoSelectedPilot();
         }
     }
+
+    private void LoadTrack()
+    {
+        if (_track == 0)
+        {
+            loading.SetActive(true);
+            SceneManager.LoadScene((int)SCENES_NUMBER.DesertTrack);
+        }
+        else if (_track == 1)
+        {
+            loading.SetActive(true);
+            SceneManager.LoadScene((int)SCENES_NUMBER.WaterTomb);
+        }
+        else if (_track == 2)
+        {
+            loading.SetActive(true);
+            SceneManager.LoadScene((int)SCENES_NUMBER.RushHour);
+        }
+        else if (_track == 3)
+        {
+            loading.SetActive(true);
+            SceneManager.LoadScene((int)SCENES_NUMBER.Satellitrack);
+        }
+
+        else if (_track == 4)
+        {
+            loading.SetActive(true);
+            SceneManager.LoadScene((int)SCENES_NUMBER.InsideTheCore);
+        }
+    }
+
+    private void OpenDoor()
+    {
+        lockerColor.SetColor("_Color", Color.green);
+        foreach (var locker in lockers)
+        {
+            locker.enabled = true;
+            locker.Play();
+        }
+    }
+
+
+    protected override void Update()
+    {
+        if (lockers[0].enabled && !lockers[0].isPlaying)
+        {
+            lockersRotation.enabled = true;
+            lockersRotation.Play();
+            lockers[0].enabled = false;
+        }
+
+        if (lockersRotation.enabled && !lockersRotation.isPlaying)
+        {
+            foreach (var door in doorBunker)
+            {
+                door.enabled = true;
+                door.Play();
+            }
+            lockersRotation.enabled = false;
+            lockerColor.SetColor("_Color", Color.red);
+        }
+
+        if(doorBunker[0].enabled && !doorBunker[0].isPlaying) LoadTrack();
+
+    }
+       
 }
