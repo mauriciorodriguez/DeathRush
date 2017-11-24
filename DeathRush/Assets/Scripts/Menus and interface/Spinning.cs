4540803af603bce1a43c7257100480bfb9256683
Martín Letizia <martin.letiziaa@gmail.com>
2017-11-24 18:17:12 -0300

    Correción en funcionamiento de SearchForRace

--- DeathRush/Assets/Scripts/Menus and interface/Spinning.cs
+++ DeathRush/Assets/Scripts/Menus and interface/Spinning.cs
@@ -14,4 +14,13 @@ public class Spinning : MonoBehaviour
         else if (rotationXYZ == RotationAxes.RotY) transform.Rotate(0, speed *  Time.deltaTime, 0);
         else if (rotationXYZ == RotationAxes.RotZ) transform.Rotate(0, 0, speed * Time.deltaTime);
     }
+
+    public void RotateHologramToTrack(int num)
+    {
+         if (num == 1) transform.localEulerAngles = new Vector3(0, -153f,0);
+         if (num == 2) transform.localEulerAngles = new Vector3(0, -18.863f, 0);
+         if (num == 3) transform.localEulerAngles = new Vector3(0, 9.95f, 0);
+         if (num == 4) transform.localEulerAngles = new Vector3(0, -82.846f, 0);
+         if (num == 5) transform.localEulerAngles = new Vector3(0, -127.526f, 0);
+    }
 }
